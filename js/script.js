62b37e5917957d0b350bd4bba26f7cd1cfbdd112
roopesh-nivedit <roopeshnivedit@aol.com>
2022-04-03 12:51:25 +0530

    initial commit for website

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const downloadResume = document.getElementsByTagName("button")[0];
+
+downloadResume.addEventListener("click", function () {
+    console.log("Downloaded!!!");
+    downloadResume.style.backgroundColor = "#202538";
+    downloadResume.style.borderColor = "#202538";
+});
